310f0860958efa4052286908eb3cbecb16549e7f
martin <telemagico@gmail.com>
2017-07-22 11:08:13 -0300

    remove sensible data

---
+++ config.db.example.php
@@ -0,0 +1,8 @@
+<?php
+
+$db_source = 'database_source';
+$db_destination = 'database_app';
+$db_host = '127.0.0.1';
+$db_user = '';
+$db_pass = '';
+$brand = 'brand';
--- config.db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$db_source = 'bevforce_final';
-$db_destination = 'forcebrands_app';
-$db_host = '127.0.0.1';
-$db_user = 'root';
-$db_pass = 'F0rce7102brand.$';
-$brand = 'bevforce';
