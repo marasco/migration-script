e9993d2fab5e177802e33abd83fadea270ef7489
root <root@sneakers-dev.localdomain>
2017-07-23 21:15:45 +0000

    Change php path

--- run.sh
+++ run.sh
@@ -2,9 +2,11 @@
 cd .;
 echo "Running process";
 limit=100
-phpcommand=`whereis php`
+#phpcommand=`whereis php`
+phpcommand='/usr/bin/php'
 offset=0
-
-$phpcommand ./users.php -w files -l $offset,$limit
+#limitarg=" -l $offset,$limit"
+limitarg=""
+$phpcommand ./users.php -w files $limitarg
 
 echo "Finished";
