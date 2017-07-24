2346bb33210b6ebe6217607c72336bcaafc97a13
Francisco Marasco quiroga <francisco.marasco@gmail.com>
2017-07-23 22:00:46 -0300

    FIx script

--- run.sh
+++ run.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 cd .;
+
 echo "Running process";
 limit=100
-#phpcommand=`whereis php`
+# phpcommand=`whereis php`
 phpcommand='/usr/bin/php'
 offset=0
-#limitarg=" -l $offset,$limit"
+# limitarg=" -l $offset,$limit"
 limitarg=""
-$phpcommand ./users.php -w files $limitarg
+
+$phpcommand ./users.php $limitarg
+$phpcommand ./users_files.php
 
 echo "Finished";
