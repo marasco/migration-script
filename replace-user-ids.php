d63a72fb239e6e6504fbcad383c63e6eb99860e9
Francisco Marasco quiroga <francisco.marasco@gmail.com>
2017-07-27 00:09:19 -0300

    Add terms

--- replace-user-ids.php
+++ replace-user-ids.php
@@ -10,18 +10,25 @@
 	$users = $mysql[$db_source]->query("SET sql_mode = ''"); 
 
 /*
+
+	// users
+
 	$users = $mysql[$db_source]->query("UPDATE users SET uid = (uid + 999999) $testLimit") or die($mysql[$db_source]->error); 
 	
 	$users = $mysql[$db_source]->query("UPDATE bf_users_options SET uid = (uid + 999999) $testLimit") or die($mysql[$db_source]->error); 
 	
-*/
+	$users = $mysql[$db_source]->query("UPDATE bf_files SET nid = (nid + 999999) $testLimit") or die($mysql[$db_source]->error); */
+
+
+	// jobs
 	$users = $mysql[$db_source]->query("UPDATE node SET nid = (nid + 999999) $testLimit") or die($mysql[$db_source]->error); 
 	
 	$users = $mysql[$db_source]->query("UPDATE content_type_job SET nid = (nid + 999999) $testLimit") or die($mysql[$db_source]->error); 
 	
 	$users = $mysql[$db_source]->query("UPDATE bf_job_applications SET nid = (nid + 999999) $testLimit") or die($mysql[$db_source]->error); 
-/*
-	$users = $mysql[$db_source]->query("UPDATE bf_files SET nid = (nid + 999999) $testLimit") or die($mysql[$db_source]->error); */
+
+	$users = $mysql[$db_source]->query("UPDATE term_data SET nid = (nid + 999999) $testLimit") or die($mysql[$db_source]->error); 
+
 
 	//$users->free();
 	 
